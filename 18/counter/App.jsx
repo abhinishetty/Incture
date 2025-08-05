5da818c49bb31cb34e7fd00351ee5552c984e576
abhinishetty <168563864+abhinishetty@users.noreply.github.com>
2025-08-05 14:38:12 +0530

    Add files via upload

---
+++ 18/counter/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter'
+import './App.css'
+
+function App() {
+ return(
+  <div>
+  <h1>Redux: state management</h1>  
+  <Counter/>
+  </div>
+ )
+}
+
+export default App
